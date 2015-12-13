dba8a13b4a8a0e4348ad75a9a9aa5fcf693bb76d
OTLC <onthelinecoders@gmail.com>
2015-12-13 16:09:07 -0500

    Add Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :links do
     member do
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
       # Write to screen (put) "Dislike", then link it to the links controller and the downvote action
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
 
   root to: "links#index"
